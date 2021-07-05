db1ffa194a894633724270457600afde2cad3410
Szymng <nguyentienanh341@gmail.com>
2021-07-05 09:26:15 +0200

    Added functions to check cards and win condition

--- game.py
+++ game.py
@@ -1,10 +1,44 @@
 import deck
 
-def CardCheck(cards):
+def CardSum(cards):
     sum = 0
-    
-
+    aceNumber = 0
+    for card in cards:
+        if card == 'Ace':
+            aceNumber += 1
+        else:
+            if card == '2':
+                sum += 2
+            elif card == '3':
+                sum += 3
+            elif card == '4':
+                sum += 4
+            elif card == '5':
+                sum += 5
+            elif card == '6':
+                sum += 6
+            elif card == '7':
+                sum += 8
+            elif card == '9':
+                sum += 9
+            else:
+                sum += 10
+    for ace in range(0, aceNumber):
+        if sum < 11:
+            sum += 11
+        else:
+            sum += 1
     return sum
+
+def CheckSum(playerSum, dealerSum):
+    if abs(21 - playerSum) < abs(21 - dealerSum) and playerSum < 22 and dealerSum < 22:
+        return 'win'
+    elif playerSum > 21 and dealerSum < 22:
+        return 'lose'
+    elif abs(21 - playerSum) > abs(21 - dealerSum) and playerSum < 22 and dealerSum < 22:
+        return 'lose'
+    elif playerSum == dealerSum and playerSum < 22:
+        return 'Draw'
     
 def Game(player, dealer):
     Decks = deck.Deck()
@@ -12,21 +46,41 @@ def Game(player, dealer):
     print(player.money_)
     amount = int(input('Your bet: '))
     player.Bet(amount)
+    bets = amount*2
     playersCards = []
     dealersCards = []
-    
+    playerSum = 0
+    dealerSum = 0
+
     for i in range (0, 2):
         playersCards.append(Decks.Draw())
         dealersCards.append(Decks.Draw())
+    print(playersCards)
+    playerSum = CardSum(playersCards)
+    dealersCards = CardSum(dealersCards)
 
+    print(playerSum)
     while (not Stop):
-        decision = input('Type hit to draw or stop to stop\n').lower()
+        decision = input('Type hit to draw a card or stop to stop\n').lower()
         if decision == 'hit':
             playersCards.append(Decks.Draw())
             print(playersCards)
+            playerSum = CardSum(playersCards)
+            print(playerSum)
         elif decision == 'stop':
             Stop = True
         else:
             print("Unknown command\nType 'y' or 'n'\n")
             continue
+        if playerSum > 21:
+            Stop = True
+    areYouWinning = CheckSum(playerSum, dealerSum)
+    if areYouWinning.lower() == 'win':
+        print('You won!')
+        player.WinningMoney(bets)
+    elif areYouWinning.lower() == 'draw':
+        print("It's a draw")
+        player.WinningMoney(bets/2)
+    elif areYouWinning.lower() == 'lose':
+        print('You lost')
     return player.money_
